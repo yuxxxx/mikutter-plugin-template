4c39142f469bc81b178f66f5908e1b6388954a21
yuxxxx <marlcabbage@gmail.com>
2012-08-28 15:23:00 +0900

    modified:   template.rb version 0.2.x系のテンプレート作成。

--- template.rb
+++ template.rb
@@ -1,14 +1,9 @@
 Plugin.create :plugin_name_symbol do
-  filter_command do |menu|
-    menu[:plugin_slug] = {
-      :slug => :plugin_slug,
-      :name => 'name',
-      :condition => lambda{ |m| true },
-      :exec => lambda{ |m|
-        #execution task
-      },
-      :visible => true,
-      :role => :message }
-    [menu]
+  command(:plugin_slug,
+          name: 'name',
+          condition: lambda{ |m| true },
+          visible: true,
+          role: :timeline) do |opt|
+    #execution task
   end
 end
